d46e45f930b3b0b59da971178f95f65fb930330e
hayoung97 <rlagkdud3565@gmail.com>
2019-11-16 11:40:48 +0900

    first

---
+++ src/Member/Member.java
@@ -0,0 +1,21 @@
+package Member;
+
+public class Member {
+
+String id; //아이디
+
+String pw; //비밀번호
+
+String name; //이름
+
+String gen; //성별
+
+String birth; //생년월일
+
+String blood; //혈액형
+
+String self; //자기소개
+
+String permission;
+
+}
